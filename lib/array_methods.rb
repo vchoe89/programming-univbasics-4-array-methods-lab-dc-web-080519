45d4b3b96638b842f2d9bbaa844ed544d2ac29bb
Victor Jian Choe <vchoe89@gmail.com>
2019-07-23 01:25:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,8 @@ array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def using_sort(array)
-
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.sort
 end
 
 def using_reverse(array)
