03ccb14ad7f410f9b902ddec959947da1f9a81b3
Victor Jian Choe <vchoe89@gmail.com>
2019-07-23 01:24:47 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def using_include(array, element)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.include?(element)
-  element = "wow"
+  element = wow
 end
 
 def using_sort(array)
